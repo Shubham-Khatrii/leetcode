aafdd0988d176eb15e6859ce1edbfb43ce5afa70
Shubham-Khatrii <140044468+Shubham-Khatrii@users.noreply.github.com>
2025-07-01 11:52:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (93.27%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -8,10 +8,10 @@ class Solution(object):
         for i in range (1,n+1):
             if i%15==0:
                 a.append("FizzBuzz")
-            elif i%5==0:
-                a.append("Buzz")
             elif i%3==0:
                 a.append("Fizz")
+            elif i%5==0:
+                a.append("Buzz")
             else:
                 a.append(str(i))
         return a
